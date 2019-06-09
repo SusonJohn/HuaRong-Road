2d9b67f3b5c426c8a0f716a33f3dee25d2544f37
Suson <591016130@qq.com>
2019-06-09 17:56:05 +0800

    Signed-off-by: Suson <591016130@qq.com>

---
+++ Puzzle_HuaRongRoad/Assets/Piecing/Scripts/Block.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour {
+    public int score = 0;
+    public void OnMouseDown () {
+        //判断与空方块的距离,如果不是与空方块相邻的物体这不能交换
+        if (Vector3.Distance (transform.position, GameManager.Instance.empty) <= 3.8f) {
+            var temp = transform.position;
+            transform.position = GameManager.Instance.empty; //交换位置
+            GameManager.Instance.SwapEmpty (temp); //每次交换都需要根据空方块的位置,进行判定是否已经完成游戏
+            if (GameManager.Instance.isWin == true && GameManager.Instance.isSwaped == true) {
+                return;
+            }
+        }
+    }
+
+}
